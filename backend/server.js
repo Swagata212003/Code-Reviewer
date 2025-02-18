891b98caad6759b01db998f4a47fd1b9eba0d166
Swagata212003 <126780063+Swagata212003@users.noreply.github.com>
2025-02-18 15:39:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
-require('dotenv').config()
-const app = require('./src/app')
+// require('dotenv').config()
+// const app = require('./src/app')
 
 
-app.listen(3000, () =>{
-    console.log('Server is running')
-})  
+// app.listen(3000, () =>{
+//     console.log('Server is running')
+// })  
+
+
+require('dotenv').config();
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
